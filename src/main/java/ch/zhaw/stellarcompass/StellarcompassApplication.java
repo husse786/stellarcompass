0e16e21496a877c139e5316952d070f2bc9e0a41
husse786 <seyidhusseinh@gmail.com>
2025-09-26 02:22:44 +0200

    first commit

---
+++ src/main/java/ch/zhaw/stellarcompass/StellarcompassApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.stellarcompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarcompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellarcompassApplication.class, args);
+	}
+
+}
